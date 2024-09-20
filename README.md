bf4cb81c738d179b5a7dd932f05bbab09d744d00
seveibar <seveibar@gmail.com>
2024-09-20 15:48:22 -0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # circuit-json-to-pnp-csv
+
 Convert Circuit JSON into a Pick'n'Place CSV
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import type { AnyCircuitElement, LayerRef } from "circuit-json"
+import Papa from "papaparse"
+
+interface PickAndPlaceRow {
+  designator: string
+  mid_x: number
+  mid_y: number
+  layer: LayerRef
+  rotation: number
+}
+
+export const convertSoupToPickAndPlaceRows = (
+  soup: AnyCircuitElement[],
+  opts: { flip_y_axis?: boolean } = {},
+): PickAndPlaceRow[] => {
+  opts.flip_y_axis ??= false
+
+  const rows: PickAndPlaceRow[] = []
+  for (const element of soup) {
+    if (element.type === "pcb_component") {
+      rows.push({
+        designator: element.pcb_component_id,
+        mid_x: element.center.x,
+        mid_y: element.center.y * (opts.flip_y_axis ? -1 : 1),
+        layer: element.layer,
+        rotation: element.rotation,
+      })
+    }
+  }
+  return rows
+}
+
+export const convertSoupToPickAndPlaceCsv = (soup: AnySoupElement[]): string =>
+  Papa.unparse(
+    convertSoupToPickAndPlaceRows(soup).map((row) => ({
+      Designator: row.designator,
+      "Mid X": row.mid_x,
+      "Mid Y": row.mid_y,
+      Layer: row.layer,
+      Rotation: row.rotation,
+    })),
+  )
